48935aaee96ecf2c3f1deac562f7990e5d0ffd05
fanghr <chfanghr@gmail.com>
2019-03-25 15:43:26 +0800

    update ci configuration

--- .circleci/build_backend_arduino.sh
+++ .circleci/build_backend_arduino.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-mkdir backend_arduino_build
-cd backend_arduino_build;cmake cmd/backsrv/backend_arduino/
-cd backend_arduino_build;make -C backend_arduino_build -j4
+cd build;cmake ../cmd/backsrv/backend_arduino/
+cd build;make -j4
