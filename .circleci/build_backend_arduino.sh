4abe5b75aa4549cb1cc9c51e243e49b18d03468f
fanghr <chfanghr@gmail.com>
2019-03-25 15:05:26 +0800

    add arduino ci configuration

---
+++ .circleci/build_backend_arduino.sh
@@ -0,0 +1,1 @@
+arduino --verify --board arduino:avr:nano $PWD/cmd/backsrv/backend_arduino/backend_arduino.ino
---
+++ .circleci/install_arduino_toolchain.sh
@@ -0,0 +1,5 @@
+wget http://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
+tar xf arduino-1.8.8-linux64.tar.xz
+sudo mv arduino-1.8.8 /usr/local/share/arduino
+sudo ln -s /usr/local/share/arduino/arduino /usr/local/bin/arduino
+#sudo apt install cmake
