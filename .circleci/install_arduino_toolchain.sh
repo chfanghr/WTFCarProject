e6652fb8386dfd9057bb9107fe707008e6c28177
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-03-26 11:00:56 +0800

    Update install_arduino_toolchain.sh

--- .circleci/install_arduino_toolchain.sh
+++ .circleci/install_arduino_toolchain.sh
@@ -4,5 +4,5 @@ wget -q http://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
 tar xf arduino-1.8.8-linux64.tar.xz
 sudo mv arduino-1.8.8 /usr/local/share/arduino
 sudo ln -s /usr/local/share/arduino/arduino /usr/local/bin/arduino
-sudo apt -qq install -y cmake curl
+sudo apt -qq install -y cmake curl >> /dev/null
 rm -rf arduino*
