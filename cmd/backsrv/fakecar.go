1393ddd6d79ace766c4b9fb3f4a80fcbe4376ee4
fanghr <chfanghr@gmail.com>
2019-03-28 09:54:56 +0800

    accomplish implementation of fakeCar

--- cmd/backsrv/fakecar.go
+++ cmd/backsrv/fakecar.go
@@ -9,25 +9,36 @@ import (
 	_ "github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"sync"
 )
 
 type FakeCar struct {
+	mu      *sync.Mutex
 	current *location.Point2D
 	//lastMovementStatus int//currently not needed
 	l        *log.Logger
 	upgrader websocket.Upgrader
 	wss      wsService
 }
+type message struct {
+	CurrentLocation  *location.Point2D `json:"cur,omitempty"`
+	DestineLocation  *location.Point2D `json:"dest,omitempty"`
+	StopMovementFlag bool              `json:"stop,omitempty"`
+}
 
 func NewFakeCar(l *log.Logger, listenAddr string) *FakeCar {
-	res := &FakeCar{}
+	res := &FakeCar{mu: new(sync.Mutex)}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		ws, err := res.upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			l.Println("error upgrade ws", err)
 			return
 		}
-		res.wss.AddConnection(newWsConnection(l, ws))
+		conn := newWsConnection(l, ws)
+		res.mu.Lock()
+		_ = conn.WriteJSON(message{CurrentLocation: res.current})
+		res.mu.Unlock()
+		res.wss.AddConnection(conn)
 	})
 	go l.Fatalln(http.ListenAndServe(listenAddr, http.DefaultServeMux))
 	res.current = new(location.Point2D)
@@ -37,15 +48,17 @@ func (f *FakeCar) GetLocation() (location.Point2D, error) {
 	return *f.current, nil
 }
 func (f *FakeCar) MoveTo(l location.Point2D) error {
-	f.l.Println("FakeCar.MoveTo() called:", l) //TODO implement this method
-	return nil
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.current = &l
+	return f.wss.Update(message{DestineLocation: &l})
 }
 func (f *FakeCar) LastMovementStatus() int {
 	//always success
 	return car.Succeeded //FIXME
 }
 func (f *FakeCar) StopMovement() error {
-	return fmt.Errorf("not implemented") //TODO implement this method
+	return f.wss.Update(message{StopMovementFlag: true})
 }
 func (f *FakeCar) IRSend(ir hardware.IRData) error {
 	//need no implementation because there's no simulation
